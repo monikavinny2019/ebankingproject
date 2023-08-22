15c4b871a716396b17cd192fb22010e17073b082
monikavinny2019 <monikavinny2019@gmail.com>
2023-08-22 20:37:39 +0530

    compiler changes

---
+++ src/test/java/com/ebankingprpject/TestCases/Democheck.java
@@ -0,0 +1,12 @@
+package com.ebankingprpject.TestCases;
+
+public class Democheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello check");
+
+	}
+
+}
